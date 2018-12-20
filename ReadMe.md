920f5ed4a5d9dd289738737235202e3552d28b6f
alahdekorpi <alexlahdekorpi@hotmail.com>
2018-12-20 15:04:56 +0200

    Issues #3 and #4 and readme

---
+++ ReadMe.md
@@ -0,0 +1,8 @@
+In this program, I present the following Security issues from the OWASP top 10 security issues list ( https://www.owasp.org/images/7/72/OWASP_Top_10-2017_%28en%29.pdf.pdf ) :
+1. SQL injection.
+2. Broken authentication.
+3. Broken access control.
+4. Sensitive data exposure.
+5. Insufficient logging.
+
+
--- src/main/java/sec/project/controller/SignupController.java
+++ src/main/java/sec/project/controller/SignupController.java
@@ -1,7 +1,10 @@
 package sec.project.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,11 +17,6 @@ public class SignupController {
     @Autowired
     private SignupRepository signupRepository;
 
-    @RequestMapping("*")
-    public String defaultMapping() {
-        return "redirect:/form";
-    }
-
     @RequestMapping(value = "/form", method = RequestMethod.GET)
     public String loadForm() {
         return "form";
@@ -29,5 +27,18 @@ public class SignupController {
         signupRepository.save(new Signup(name, address));
         return "done";
     }
-
+    
+    // SECURITY PROBLEMS #4 && #5
+    @RequestMapping(value = "/signups")
+    public String listAll(Model model) {
+        model.addAttribute("signups", signupRepository.findAll());
+        return "signups";
+    }
+    
+    @RequestMapping("*")
+    public String defaultMapping() {
+        return "redirect:/form";
+    }
+        
 }
+    
